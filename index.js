9b99ecf0c5570c963843881fcf554a790d2f83e0
wmq <1214520313@qq.com>
2018-01-29 20:40:38 +0800

    首页组件静态

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import ReactDOM from 'react-dom';
 import React from 'react'
 // router
 import { HashRouter as Router, Route, Link} from 'react-router-dom'
-import "weui";
 // redux
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 //选项卡组件
 import Xindex from "./components/tab/xindex.jsx";
-/*import Xdetail from "./components/tab/xdetail.jsx";*/
+import Xpicturedetail from "./components/tab/xpicturedetail.jsx";
 
 const store = createStore((state={
     name:"测试",
@@ -35,7 +34,8 @@ ReactDOM.render(
     <Provider store={store}>
         <Router>
             <div>
-                <Route path="/index" component={Xindex}></Route>
+                <Route path="/" component={Xindex}></Route>
+                <Route path="/picdetail" component={Xpicturedetail}></Route>   
             </div>
         </Router>
     </Provider>
